744efaa8853eb46076b3e08e44f4f18b0333b8a4
Yuliia-Bakalova <98843051+Yuliia-Bakalova@users.noreply.github.com>
2023-01-17 15:36:40 +0200

    done

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # -goit-node-hw-01
+
+action="list" : https://ibb.co/c8jbTMW ;
+action="get" : https://ibb.co/n36bkdw ;
+action="add" : https://ibb.co/FbWbM7C ;
+action="remove" : https://ibb.co/sV5yB4j ;
---
+++ index.js
@@ -0,0 +1,52 @@
+const { addContact, removeContact, listContacts, getContactById,} = require("./contacts");
+const { Command } = require("commander");
+const program = new Command();
+
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+const argv = program.opts();
+
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      console.log("invoke list");
+      const contacts = await listContacts();
+      console.table(contacts);
+      break;
+
+    case "get":
+      const current = await getContactById(id);
+      console.log(current);
+      break;
+
+    case "add":
+      await addContact(name, email, phone);
+      const newContactsList = await listContacts();
+      console.log(
+        `Contact : ${name}, ${email}, ${phone} was successfully added`
+      );
+      console.table(newContactsList);
+      break;
+
+    case "remove":
+      await removeContact(id);
+      const newContacts = await listContacts();
+      console.log(`Contact with id ${id} was removed`);
+      console.table(newContacts);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+
+invokeAction(argv);
